e2762bbe5f985fe9b4ec174976b295b6eaafc9bb
Jeremy Poulter <Jpoulter@gmail.com>
2019-12-01 19:17:47 -0700

    add script functionality

--- main.js
+++ main.js
@@ -1,6 +1,30 @@
 var closeBox = document.querySelector("#XB");
 var closeThis = document.querySelector("#RmvBanner");
+
 closeBox.addEventListener('click', exitBanner);
+
 function exitBanner() {
   closeThis.parentNode.removeChild(closeThis);
+}
+
+var walletIcon = document.querySelector("#icon2");
+var clipboardIcon = document.querySelector("#icon1");
+var dashboardPage = document.querySelector(".clipboard");
+var transactionPage = document.querySelector(".transaction-page");
+
+walletIcon.addEventListener('click', changeToTransactions);
+clipboardIcon.addEventListener('click', changeToDashboard);
+
+function changeToTransactions () {
+  clipboardIcon.classList.remove('turq-highlight');
+  walletIcon.classList.add('turq-highlight');
+  dashboardPage.classList.add('hideMe');
+  transactionPage.classList.remove('hideMe');
+}
+
+function changeToDashboard () {
+  clipboardIcon.classList.add('turq-highlight');
+  walletIcon.classList.remove('turq-highlight');
+  dashboardPage.classList.remove('hideMe');
+  transactionPage.classList.add('hideMe');
 }
